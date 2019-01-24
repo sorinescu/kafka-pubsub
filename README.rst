ac213d2823a278b7c145605226a4809086d703cc
Sorin Otescu <sorinu@gmail.com>
2019-01-24 12:41:08 +0200

    Set up project layout

---
+++ README.rst
@@ -0,0 +1,43 @@
+kafka-pubsub
+============
+
+Description
+-----------
+
+Kafka to Google PubSub connector.
+
+.. image:: https://travis-ci.org/rpappalax/python-project-template.svg?branch=dev
+    :target: https://travis-ci.org/rpappalax/python-project-template
+
+.. image:: https://badge.fury.io/py/python-project-template.svg
+    :target: http://badge.fury.io/py/python-project-template
+
+.. image:: https://coveralls.io/repos/rpappalax/python-project-template/badge.svg
+    :target: https://coveralls.io/r/rpappalax/python-project-template
+
+
+Install
+-------
+
+Directly from PyPI:
+
+.. code:: bash
+
+    $ pip install kafka-pubsub
+
+Or locally, in development mode:
+
+.. code:: bash
+
+    $ git clone https://github.com/sorinescu/kafka-pubsub.git
+    $ cd kafka-pubsub
+    $ pip install -e .
+
+
+Reference
+---------
+
+- `<https://github.com/dpkp/kafka-python>`_
+- `<https://cloud.google.com/pubsub/docs/reference/libraries#client-libraries-install-python>`_
+- `<https://tox.readthedocs.org/en/latest/>`_
+- `<https://travis-ci.org/>`_
---
+++ setup.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, 'README.rst')) as f:
+    README = f.read()
+
+REQUIREMENTS = [
+    'kafka-python>=1.4.4'
+]
+
+TEST_REQUIREMENTS = [
+    'flake8>=2.4.0',
+    'pytest',
+    'pytest-cov',
+    'Sphinx>=1.3',
+    'sphinx_rtd_theme>=0.1.7',
+    'tox>=1.9.0'
+]
+
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = [
+            '--strict',
+            '-vv',
+            '--tb=long',
+            'tests']
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
+
+
+setup(
+    name='kafka-pubsub',
+    version='0.1',
+    description='kafka-pubsub',
+    long_description=README,
+    author='Sorin Otescu',
+    author_email='sorin.otescu@gmail.com',
+    url='https://github.com/sorinescu/kafka-pubsub',
+    license="MIT",
+    install_requires=REQUIREMENTS,
+    tests_require=TEST_REQUIREMENTS,
+    keywords=['kafka', 'pubsub', 'google', 'cloud'],
+    packages=find_packages(),
+    cmdclass={'test': PyTest},
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Natural Language :: English',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Operating System :: POSIX :: Linux',
+        'Topic :: System :: Networking'
+    ],
+    entry_points={
+        'console_scripts': ['demo = demo.demo_handler:main']
+    },
+)
